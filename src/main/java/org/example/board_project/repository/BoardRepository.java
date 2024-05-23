ac6db127eb28727770958f8b1d0579cb1827f343
h_jinnny <nd3807@naver.com>
2024-05-23 17:51:48 +0900

    BoardRepository 인터페이스 생성

--- src/main/java/org/example/board_project/repository/BoardRepository.java
+++ src/main/java/org/example/board_project/repository/BoardRepository.java
@@ -1,6 +1,7 @@
 package org.example.board_project.repository;
 
+import org.example.board_project.domain.Board;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BoardRepository extends CrudRepository {
+public interface BoardRepository extends CrudRepository<Board, Long> {
 }
