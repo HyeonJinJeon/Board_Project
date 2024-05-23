a41ef23d7f925cbf6bee36b1c72acbb48cfd3882
h_jinnny <nd3807@naver.com>
2024-05-23 17:51:03 +0900

    게시글 리스트, 상세 정보, 수정 컨트롤러 작성

--- src/main/java/org/example/board_project/controller/BoardController.java
+++ src/main/java/org/example/board_project/controller/BoardController.java
@@ -1,26 +1,47 @@
 package org.example.board_project.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.board_project.domain.Board;
 import org.example.board_project.service.BoardService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/board")
+//@RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
     //목록 보기
     @GetMapping("/list")
     public String list(Model model){
-        model.addAttribute();
+        Iterable<Board> boards =  boardService.findAllPost();
+        model.addAttribute("boards", boards);
+        return "board/list";
     }
 
     //상세 보기
+    @GetMapping("/view/{id}")
+    public String detail(@PathVariable Long id, Model model){
+        Board board = boardService.findById(id);
+        model.addAttribute("board", board);
+        return "board/detail";
+    }
 
     //글 쓰기 --요청 2개
+    @GetMapping("updateform/{id}")
+    public String updateForm(@PathVariable Long id, Model model){
+        Board board = boardService.findById(id);
+        model.addAttribute("board", board);
+        return "board/updateform";
+    }
+
+    @PostMapping("/updateform")
+    public String updaate(@ModelAttribute Board board){
+        boardService.updateBoard(board);
+        return "redirect:/list";
+    }
 
     //수정  --요청 2개
 
