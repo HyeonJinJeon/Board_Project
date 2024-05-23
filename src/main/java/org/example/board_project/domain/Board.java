1d2d10f71a43d5b1004b1aa5d07bd71182be9133
h_jinnny <nd3807@naver.com>
2024-05-23 17:50:25 +0900

    Board 도메인 생성

--- src/main/java/org/example/board_project/domain/Board.java
+++ src/main/java/org/example/board_project/domain/Board.java
@@ -1,10 +1,21 @@
 package org.example.board_project.domain;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Board {
+    @Id
     private Long id;
     private String name;
     private String title;
+    private String content;
     private LocalDate createdAt;
 }
